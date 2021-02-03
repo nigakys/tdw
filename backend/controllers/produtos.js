a92631250b207b88ba12a755530ef1a015909ab8
RandomCodePew <nigakys@gmail.com>
2021-02-03 10:36:20 +0000

    first commit

---
+++ backend/controllers/produtos.js
@@ -0,0 +1,67 @@
+const { isAuthorized } = require("../middleware/auth.js");
+var mongo = require('mongodb');
+
+module.exports = function (app, collections) {
+    var users = collections["users"];
+    var items = collections["items"];
+
+    app.use((req, res, next) => {
+        users.find().toArray().then(result => {
+            isAuthorized(req, res, next, result);
+        })
+    });
+
+    app.get('/', (req, res) => {
+        items.find().toArray().then(result => {
+            res.status(200).send(result);
+        })
+    });
+
+    app.get('/:id', (req, res) => {
+        var id = new mongo.ObjectID(req.params.id);
+        var query = { _id: id };
+        items.findOne(query).then(result => {
+            res.status(200).send(result);
+        })
+    });
+
+    app.post('/', (req, res) => {
+        items.insertOne(req.body, function (err, res) {
+            if (err) res.status(400).send("Erro a inserir");
+            else res.stauts(200).send("Prato inserido" + req.body); res.stauts(200).send("Prato inserido" + req.body);
+        });
+
+    })
+
+    app.put('/:id', (req, res) => {
+        var id = new mongo.ObjectID(req.params.id);
+        var query = { _id: id };
+        var values = { $set: req.body };
+
+        items.updateOne(query, values, function (err, res) {
+            if (err) throw err;
+        });
+        res.status(200).send("Item atualizado com sucesso");
+    })
+
+    app.delete('/:id', (req, res) => {
+        var id = new mongo.ObjectID(req.params.id);
+        var query = { _id: id };
+
+        items.deleteOne(query, function (err, res) {
+            if (err) {
+                return err;               
+            }
+        });
+        res.status(200).send("Item apagado com sucesso");
+    })
+
+    app.delete('/', (req, res) => {
+        items.deleteMany(function (err, res) {
+            if (err) {
+                return err;
+            }
+        });
+        res.status(200).send("Todos os items foram apagados")
+    })
+}
---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+app.use(express.json());
+
+var MongoClient = require('mongodb').MongoClient;
+
+var url = "mongodb://pcarmo:pcarmo123@cluster-shard-00-00.jabin.mongodb.net:27017,cluster-shard-00-01.jabin.mongodb.net:27017,cluster-shard-00-02.jabin.mongodb.net:27017/db?ssl=true&replicaSet=atlas-610jb3-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+MongoClient.connect(url, {useUnifiedTopology: true}, function (err, client) {
+	if (err == null) {
+		let collections = {
+			"users": client.db("Db").collection("users"),
+			"items": client.db("Db").collection("items")
+		}
+
+		require("./controllers/pratosDia")(app, collections);
+
+		app.listen(port, () => console.log('App | Pedro Carmo'));
+	}
+});
